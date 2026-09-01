f79e72e4415a8ddf6e73022d47b3341ee1c24c0d
ahmadanggra <ahmadanggra@gmail.com>
2026-09-01 05:22:08 +0000

    update sample to use learn instead parse

--- pyats/pyats_example.py
+++ pyats/pyats_example.py
@@ -6,9 +6,8 @@ device = testbed.devices['csr1']
 device.connect(log_stdout=False)
 
 try:
-    output = device.parse('show ip ospf neighbor')
-    json_output = json.dumps(output, indent=4)
-    print(json_output)
+    output = device.learn('ospf')
+    print(json.dumps(output.to_dict(), indent=4))
 except Exception as e:
     print(f"Error parsing command: {e}")
 finally:
