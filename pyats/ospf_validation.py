9ddc17e5beb31f566b91052a37dcff2fd49bd144
ahmadanggra <ahmadanggra@gmail.com>
2026-09-01 05:51:36 +0000

    output not recognize by genie diff

--- pyats/ospf_validation.py
+++ pyats/ospf_validation.py
@@ -1,24 +1,40 @@
 #!/usr/bin/env python3
 import json
+import sys
 from pathlib import Path
+
 from pyats.topology import loader
 
-try:
-    testbed = loader.load('testbed.yml')
-    learnt = {}
-    for name, dev in testbed.devices.items():
-        output_dir = Path('./pre-change/')
-        output_dir.mkdir(parents=True, exist_ok=True)
-        dev.connect(log_stdout=False)
-        learnt[name] = {}
-        learnt[name]['routing'] = dev.learn('routing')
-        learnt[name]['ospf'] = dev.learn('ospf')
-        # print(json.dumps(learnt[name]['routing'].to_dict(), indent=4))
-        # print(json.dumps(learnt[name]['ospf'].to_dict(), indent=4))
-        with open(f"{output_dir}/{name}_routing.json", "w") as f:
-            json.dump(learnt[name]['routing'].to_dict(), f, indent=4)
-        with open(f"{output_dir}/{name}_ospf.json", "w") as f:
-            json.dump(learnt[name]['ospf'].to_dict(), f, indent=4)
-        dev.disconnect()
-except Exception as e:
-    print(f"Error parsing command: {e}")
+
+def validate_ospf(output_dir):
+    output_path = Path('./' + output_dir + '/')
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    try:
+        testbed = loader.load('testbed.yml')
+        learnt = {}
+
+        for name, dev in testbed.devices.items():
+            dev.connect(log_stdout=False)
+            learnt[name] = {}
+            learnt[name]['routing'] = dev.learn('routing')
+            learnt[name]['ospf'] = dev.learn('ospf')
+
+            with open(output_path / f'{name}_routing.json', 'w') as f:
+                json.dump(learnt[name]['routing'].to_dict(), f, indent=4)
+
+            with open(output_path / f'{name}_ospf.json', 'w') as f:
+                json.dump(learnt[name]['ospf'].to_dict(), f, indent=4)
+
+            dev.disconnect()
+
+    except Exception as e:
+        print(f"Error parsing command: {e}")
+
+
+def main():
+    validate_ospf(sys.argv[1])
+
+
+if __name__ == '__main__':
+    main()
